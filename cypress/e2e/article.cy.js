0793d8ff904ec8f09af8f62fec0784c04cafae46
Александр Ляшенко <sanko.lialko14@gmail.com>
2025-04-22 14:01:22 +0300

    d

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,13 +1,53 @@
-describe('', () => {
+const { faker } = require('@faker-js/faker');
+
+describe('Create article', () => {
+  let user;
+  let article;
+
   before(() => {
-
+    cy.task('generateUser').then((generatedUser) => {
+      user = generatedUser;
+      cy.login(user.email, user.username, user.password);
+    });
   });
 
-  it('', () => {
+  it('should allow to create an article', () => {
+    article = {
+      title: faker.lorem.words(3),
+      description: faker.lorem.sentence(),
+      body: faker.lorem.paragraphs(2)
+    };
 
+    cy.createArticle(article.title, article.description, article.body);
+    cy.contains('nav-link', 'New Article').click();
+
+    cy.get('[placeholder="Article Title"]').type(article.title);
+    cy.get(`[placeholder="What's this article about?"]`).type(
+      article.description
+    );
+    // eslint-disable-next-line max-len
+    cy.get('[placeholder="Write your article (in markdown)"]').type(
+      article.body
+    );
+
+    cy.get('.btn').click();
+
+    cy.contains(article.title);
   });
 
-  it('', () => {
+  it('should allow to delete articles', () => {
+    article = {
+      title: faker.lorem.words(3),
+      description: faker.lorem.sentence(),
+      body: faker.lorem.paragraphs(2)
+    };
 
+    cy.createArticle(article.title, article.description, article.body);
+
+    cy.contains(article.title).click();
+
+    cy.contains('button', 'Delete Article').click();
+
+    cy.contains('No articles are here... yet.');
   });
 });
